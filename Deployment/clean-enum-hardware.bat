488c5ec6cc0b0a3e8761d4dce5ccbc9e3ad36184
skulltrail <samuel_4186@homail.com>
2024-05-29 15:43:13 -0300

    Fix for No audio device and other issues after installing windows with new Setup engine.

--- Deployment/clean-enum-hardware.bat
+++ Deployment/clean-enum-hardware.bat
@@ -19,7 +19,35 @@ REM "%~dp0tools\SetAcl\SetACL.exe" -on "HKEY_LOCAL_MACHINE\TempSytem\ControlSet0
 REM "%~dp0tools\SetAcl\SetACL.exe" -on "HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum" -ot reg -actn ace -ace "n:Administrators;p:full"
 REM "%~dp0tools\SetAcl\subinacl\subinacl.exe" /keyreg HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum /grant=Administrators
 
-reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum /f
-reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\ACPI /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\ACPI_HAL /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\BTH /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\HID /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\HTREE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\IDE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\ISAPNP /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\LEGACY /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\PCI /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\PCIIDE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\Root\ACPI_HAL /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\Root\COMPOSITE_BATTERY /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\STORAGE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\SW /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet001\Enum\USB /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\ACPI /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\ACPI_HAL /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\BTH /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\HID /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\HTREE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\IDE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\ISAPNP /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\LEGACY /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\PCI /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\PCIIDE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\Root\ACPI_HAL /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\Root\COMPOSITE_BATTERY /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\STORAGE /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\SW /f
+reg delete HKEY_LOCAL_MACHINE\TempSytem\ControlSet002\Enum\USB /f
 
 reg unload HKLM\TempSytem >nul	
